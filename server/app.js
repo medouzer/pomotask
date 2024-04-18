af57d4720337782fa9cd97528376bb712048d7bc
medouzer <96297689+medouzer@users.noreply.github.com>
2024-04-18 23:37:16 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://pomotask.vercel.app'],
+    origin: ['https://pomotask-ten.vercel.app', 'pomotask-ten.vercel.app'],
     credentials: true,
 }));
 
