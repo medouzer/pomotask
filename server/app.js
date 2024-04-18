b7595df027a394d8e890f88c4b370aa63f365d58
medouzer <96297689+medouzer@users.noreply.github.com>
2024-04-18 23:58:25 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,12 +8,18 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/v1', require('./src/v1/routes'));
+app.use('/api/v1', cors(), require('./src/v1/routes'));
 
 module.exports = app;
