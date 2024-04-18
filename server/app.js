b5adab7515055ede9745886eb460e4afbf86dc47
medouzer <96297689+medouzer@users.noreply.github.com>
2024-04-19 00:01:15 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,8 +6,6 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
